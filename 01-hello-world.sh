2ddb3670b8defc3b310cc9d3605550a4e9c73762
sundar <sundarsodadasi1996@gmail.com>
2024-10-30 16:29:23 +0530

    third commit

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 echo "Hi Hello World, I'm learning DevOps with AWS"
+PERSON1=Sachin
+PERSON2=Rohit
 
 
-echo "Ramesh: Hi Suresh, good morning"
-echo "Suresh: good morning Ramesh"
-echo "Ramesh: how are you suresh"
-echo "Suresh: I'm good Ramesh"
+echo "$PERSON1: Hi $PERSON2, good morning"
+echo "$PERSON2: good morning $PERSON1"
+echo "$PERSON1: how are you $PERSON2"
+echo "$PERSON2: I'm good $PERSON1"
