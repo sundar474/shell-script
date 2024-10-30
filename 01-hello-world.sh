df73914dfbf63754e3d530c04707e704fc7729f6
sundar <sundarsodadasi1996@gmail.com>
2024-10-30 16:23:51 +0530

    second commit

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 echo "Hi Hello World, I'm learning DevOps with AWS"
+
+
+echo "Ramesh: Hi Suresh, good morning"
+echo "Suresh: good morning Ramesh"
+echo "Ramesh: how are you suresh"
+echo "Suresh: I'm good Ramesh"
